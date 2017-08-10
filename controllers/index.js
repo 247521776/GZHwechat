3101296b073dce9b826bc81fc61aec4cab2733dc
boom <wo247521776@qq.com>
2017-08-10 14:45:48 +0800

    微信公众号

--- controllers/index.js
+++ controllers/index.js
@@ -2,13 +2,13 @@
  * Created by boom on 2017/8/9.
  */
 const router = require("express").Router();
+const reply = new require("../models/reply");
 const config = require("../config.json").wechat;
 const wechat = require("wechat");
 
 router.all("/api/reply", wechat(config, (req, res, next) => {
     const message = req.weixin;
     console.log(message, "----");
-    const reply = require("../models/reply");
     reply.findAll(message.Content, function(err, data) {
         console.log(err, data);
         if (err) {
