691fe75fc4cc194f1eef10e3b17266de36b25216
boom <wo247521776@qq.com>
2017-11-02 13:57:20 +0800

    测试jenkins

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const models      = join(__dirname, "./models");
 const bodyParser  = require("body-parser");
 const cors        = require("cors");
 mongoose.Promise = global.Promise;
+console.log("====");
 
 app.use(cors());
 
